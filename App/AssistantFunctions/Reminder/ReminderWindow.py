9baff338f0746554a201b5b7302990d5aa5a9115
SharinAlex <alex.shavrin200303@gmail.com>
2023-07-07 17:53:19 +0300

    ShavrinAlex: Refactoring (added documentation)

--- App/AssistantFunctions/Reminder/ReminderWindow.py
+++ App/AssistantFunctions/Reminder/ReminderWindow.py
@@ -1,18 +1,30 @@
 import tkinter as tk
 import tkinter.messagebox as mb
 
+
 class ReminderWindow(tk.Tk):
-    def __init__(self, events_to_remind: list):
+    """
+    Класс графического представления напоминаний
+    Сlass of graphical representation of reminders
+    """
+
+    def __init__(self, events_to_remind: list) -> None:
         self.__remind_title = "Напоминание"
         self.__window_title = "Новое уведовмление!"
         super().__init__()
-        btn_info = tk.Button(self, text=self.__window_title, command= lambda: 
-                                self.open_reminds(events_to_remind))
+        btn_info = tk.Button(self, text=self.__window_title, command=lambda:self.open_reminds(events_to_remind))
         opts = {'padx': 40, 'pady': 5, 'expand': True, 'fill': tk.BOTH}
         btn_info.pack(**opts)
-        pass
 
     def open_reminds(self, events_to_remind: list) -> None:
+        """
+        Метод, открывающий напоминание
+        The method that opens the reminder
+
+        :param events_to_remind: list - список событий для напоминания
+        :param events_to_remind: list - list of events to remind
+        """
+
         for event in events_to_remind:
             event_message = event[0]
             event_date = event[1]
@@ -21,12 +33,3 @@ class ReminderWindow(tk.Tk):
             output_string = "{} \n {} \n {}".format(event_message, event_date, event_remind_time_for_output)
             mb.minsize(400, 80)
             mb.showinfo(self.__remind_title, output_string)
-        pass
-
-if __name__ == "__main__":
-    msg = "День рождение Васи"
-    date = "7 июля"
-    remind_time = 1
-    window = ReminderWindow([[msg,date,remind_time], ["Уйти вечером в запой по заветам Рыбина", "7 июля", 2]])
-    window.mainloop()
-    pass
