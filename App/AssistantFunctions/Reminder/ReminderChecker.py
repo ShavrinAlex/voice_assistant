a8fd3ded97932a94db980079c3f762e36dde53ab
Dmitriy <d.deshura@yandex.ru>
2023-07-09 15:58:51 +0300

    DeshuraDmitriy: bag unrecognized variable in  RemindChecker fixed?

--- App/AssistantFunctions/Reminder/ReminderChecker.py
+++ App/AssistantFunctions/Reminder/ReminderChecker.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from App.AssistantFunctions.Reminder.ReminderWindow import ReminderWindow
+from App.PickleLoader.PickleLoader import PickleLoader
 import pickle
 
 
@@ -12,27 +13,27 @@ class ReminderChecker:
 
     def check_events(self):
         events_to_delete = []
-        with open(EVENT_STORAGE, 'rb') as read_file:
-            try:
-                event_storage = pickle.load(read_file)
-                if not event_storage:
-                    event_storage = dict()
-                for key in event_storage.keys():
-                    if event_storage[key]["remind_from"] <= datetime.today():
-                        self.notify(key, event_storage[key])
-                    elif event_storage[key]["remind_until"] <= datetime.today():
-                        events_to_delete.append(key)
-                
-                reminds_outputer = ReminderWindow(self.__notify_list)
-                reminds_outputer.mainloop()
+        event_storage = dict()
+        try:
+            event_storage = PickleLoader.load(EVENT_STORAGE)
+            if not event_storage:
+                event_storage = dict()
+            for key in event_storage.keys():
+                if event_storage[key]["remind_from"] <= datetime.today():
+                    self.notify(key, event_storage[key])
+                elif event_storage[key]["remind_until"] <= datetime.today():
+                    events_to_delete.append(key)
+            
+            reminds_outputer = ReminderWindow(self.__notify_list)
+            reminds_outputer.mainloop()
 
-                for event in events_to_delete:
-                    del(event_storage[event])
-                    # pass
-            except:
-                pass
-        with open(EVENT_STORAGE, 'wb') as write_file:
-            pickle.dump(event_storage, write_file)
+            for event in events_to_delete:
+                del(event_storage[event])
+                # pass
+        except:
+            print("reminder exception")
+            pass
+        PickleLoader.dump(event_storage, EVENT_STORAGE)
     
     def notify(self, promt_text: str, promt_data: dict):
         self.__notify_list.append([promt_text, promt_data["remind_until"].strftime("%d %m %Y"), promt_data["remind_range"]])
